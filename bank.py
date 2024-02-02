30e030ab123dfb42dede6f30a897c91c04efd7a2
Админ <iuy.nickolaencko@gmail.com>
2024-02-02 19:39:27 +0000

    week2

---
+++ bank.py
@@ -0,0 +1,8 @@
+#Enter amount1(in cent): 65
+#Enter amount2(in cent): 180
+#The sum of these is €2.45
+
+amount1 = int(input("Enter amount1(in cent):"))
+amount2= int(input("Enter amount2(in cent):"))
+sum = amount1 + amount2
+print("The sum of these is €",sum / 100)
