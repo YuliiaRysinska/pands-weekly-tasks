bb4fa27c71410cab1b989eba0aa76ce00ad31c0d
Админ <iuy.nickolaencko@gmail.com>
2024-02-06 20:15:30 +0000

    week03

---
+++ 3.accounts.py
@@ -0,0 +1,10 @@
+# Bank account numbers can stored as 10 character strings, for security reasons some applications only display the last 4 characters (with the other other characters replaced with Xs).
+# Prog that reads in a 10 character account number and outputs the account number with only the last 4 digits showing 
+#(and the first 6 digits replaced with Xs).
+
+# bank_ac = input("Please enter an 10 digit account number:")ce[-4:0] = "X")
+
+#normalised_string = input("Please enter an 10 digit account number:")
+#normalised_string = len().replace[0:6] = "X"
+#print(normalised_string)
+#print(f"The account number:{}")
