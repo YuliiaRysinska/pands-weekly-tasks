93cf031612dd343360bf6dea8767e47f31a0202e
Админ <iuy.nickolaencko@gmail.com>
2024-02-14 13:53:42 +0000

    week04

--- 3.accounts.py
+++ 3.accounts.py
@@ -1,10 +1,12 @@
-# Bank account numbers can stored as 10 character strings, for security reasons some applications only display the last 4 characters (with the other other characters replaced with Xs).
+# Bank account numbers can stored as 10 character strings, for security reasons some applications only display the last 4 characters 
+# (with the other characters replaced with Xs).
 # Prog that reads in a 10 character account number and outputs the account number with only the last 4 digits showing 
 #(and the first 6 digits replaced with Xs).
 
-# bank_ac = input("Please enter an 10 digit account number:")ce[-4:0] = "X")
+a = input("Please enter an 10 digit account number: ")
+account_no = [1,2,3,4,5,6,7,8,9,0]
+account_no[0:6] = "X" * 6
+account_no[6:11] = a[6:11]
+print(*account_no, sep='')
 
-#normalised_string = input("Please enter an 10 digit account number:")
-#normalised_string = len().replace[0:6] = "X"
-#print(normalised_string)
-#print(f"The account number:{}")
+#Extra: Modify the program to deal with account numbers of any length (yes that is a vague requirement, comment your assumptions) 
---
+++ 4.collatz.py
@@ -0,0 +1,26 @@
+#Write a program that asks the user to input any positive integer and outputs the successive values of the following calculation.
+#At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, 
+# multiply it by three and add one.
+#Have the program end if the current value is one. Example $ python collatz.py Please enter a positive integer: 10  10 5 16 8 4 2 1
+
+# Entering a positive integer by user.
+n = []
+n = int(input("Please enter a positive integer: "))
+
+# The condition for this loop is n != 1, so it will continue until n is 1, which will make the condition false. 
+while n != 1:
+    print (n, end=' ')
+    # The script will start producing a decimal during the loop while inputting a positive integer -> integer division with // that always give a int    
+    if n % 2 == 0: 
+        # n is even
+        n = n // 2   
+    else:
+        # n is odd  
+        n = n * 3 + 1   
+else:
+    print (n, end=' ')          
+    
+    
+    
+    
+    
