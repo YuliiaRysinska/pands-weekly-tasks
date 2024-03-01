3c954430310dc7b7ab55d4ae6f3d1d52b7ffb5f9
Админ <iuy.nickolaencko@gmail.com>
2024-03-01 13:35:29 +0000

    defining weekday/weekend + conditions,if,else

--- 5.weekday.py
+++ 5.weekday.py
@@ -1,11 +1,13 @@
 # program that outputs whether or not today is a weekday. need to search the web to find how you work out what day it is.
 # An example of running this program on a Thursday is : Yes, unfortunately today is a weekday. It is the weekend, yay!
-#days = ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')
 
+#import a module "datetime" to work with dates as date objects
 import datetime
 today = datetime. datetime. today() 
+#print(today)
 
+# using conditions, if,else
 if today. weekday(): 
-    print("Yes, unfortunately today is a weekday")
+   print("Yes, unfortunately today is a weekday")
 else:
-    print("It is the weekend, yay!")
+   print("It is the weekend, yay!")
