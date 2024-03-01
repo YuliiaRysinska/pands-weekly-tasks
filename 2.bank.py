8c0fe0cf78c80f0317ad5ffe9c32402745e1e017
Админ <iuy.nickolaencko@gmail.com>
2024-03-01 13:10:41 +0000

    put 2 amount in cent and count in euro

--- 2.bank.py
+++ 2.bank.py
@@ -1,7 +1,7 @@
-#Enter amount1(in cent): 65
-#Enter amount2(in cent): 180
-#The sum of these is €2.45
-
+# Program should:
+#Prompt the user and read in two money amounts (in cent)
+#Add the two amounts
+#Print out the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount 
 amount1 = int(input("Enter amount1(in cent):"))
 amount2= int(input("Enter amount2(in cent):"))
 sum = amount1 + amount2
